ad0f7162ae037bdab926a0052b496a95100c1244
Aleksei Elin <leshaelin@gmail.com>
2018-05-31 22:06:38 +0300

    Concept

---
+++ CodeJam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodeJam
+//
+//  Created by LESHA on 5/31/18.
+//  Copyright © 2018 Lesha Elin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CodeJam/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CodeJam
+//
+//  Created by LESHA on 5/31/18.
+//  Copyright © 2018 Lesha Elin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
